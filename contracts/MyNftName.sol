67446f795ace7caed4799c634d6b4550d2cab546
AppsusUK <suraj@appsus.co.uk>
2021-10-01 12:52:45 +0100

    Initial commit

---
+++ contracts/MyNftName.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/AppsusUK/NFT-Collection-Deploy-Guide/blob/main/contracts/ERC721Tradable.sol";
+
+contract MyNftName is ERC721Tradable {
+    constructor(address _proxyRegistryAddress)
+        ERC721Tradable("MyNFTName", "SYMBOL", _proxyRegistryAddress)
+    {}
+
+    function baseTokenURI() override public pure returns (string memory) {
+        return "ipfs://ADD_HASH_HERE/";
+    }
+
+}
