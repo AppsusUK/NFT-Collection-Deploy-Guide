778fda0c31d3a114de3642ff41e59e60c4e8937a
AppsusUK <suraj@appsus.co.uk>
2021-10-02 22:03:28 +0100

    test mintMultipleTo

--- contracts/ERC721Tradable.sol
+++ contracts/ERC721Tradable.sol
@@ -50,6 +50,15 @@ abstract contract ERC721Tradable is ContextMixin, ERC721Enumerable, NativeMetaTr
         _incrementTokenId();
     }
 
+    function mintMultipleTo(address _to, uint256 _amount) public onlyOwner {
+        require(_currentTokenId + _amount < maxSupply);
+            for (uint256 i = 1; i <= _mintAmount; i++) {
+                uint256 newTokenId = _getNextTokenId();
+                _mint(_to, newTokenId);
+                _incrementTokenId();
+            }
+    }
+
     /**
      * @dev calculates the next token ID based on value of _currentTokenId
      * @return uint256 for the next token ID
